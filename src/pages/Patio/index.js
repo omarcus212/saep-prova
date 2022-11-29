14ceba945dbbc6f507d2855a5e7c35e15b75f99b
MARCUS VINNÍCIUS RODRIGUES DE SOUZA <21276377@dsn127macprof.cfp127aluno.local>
2022-11-29 16:59:11 -0300

    saep mercado_automotivo

---
+++ src/pages/Patio/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+
+const Patio = () => {
+
+
+    return (
+
+        <div className="todo_patio">
+            <div className="card_area">
+                return api
+            </div>
+            <div className="card_area">
+                return api
+            </div>
+            <div className="card_area">
+                return api
+            </div>
+            <div className="card_area">
+                return api
+            </div>
+            <div className="card_area">
+                return api
+            </div>
+            <div className="card_area">
+                return api
+            </div>
+            <div className="card_area">
+                return api
+            </div>
+            <div className="card_area">
+                return api
+            </div>
+            
+        </div>
+    );
+
+
+
+}
+
+
+export default Patio;
